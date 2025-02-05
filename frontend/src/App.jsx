7606c64c8481ff3185a31851fdf32abdf764ee93
AbhinandanKumar001 <kumarabhinandan7860@gmail.com>
2025-02-05 11:22:17 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Toaster } from 'react-hot-toast';
+import './App.css'
+import Body from './components/Body'
+
+function App() {
+  return (
+    
+      <div className="App">
+        <Body />
+        <Toaster />
+      </div>
+    
+  );
+}
+
+export default App;
