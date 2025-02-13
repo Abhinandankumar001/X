4de4fcf50b06a727cd33fa2a1c11a92cfe165b43
Abhinandan Kumar <158548642+Abhinandankumar001@users.noreply.github.com>
2025-02-13 22:08:55 +0530

    Update constant.jsx

--- frontend/src/utils/constant.jsx
+++ frontend/src/utils/constant.jsx
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "x-six-liard.vercel.app/api/v1/user";
-export const TWEET_API_END_POINT = "x-six-liard.vercel.app/api/v1/tweet";
+export const USER_API_END_POINT = "https://x-six-liard.vercel.app/api/v1/user";
+export const TWEET_API_END_POINT = "https://x-six-liard.vercel.app/api/v1/tweet";
 
 export const timeSince = (timestamp) => {
     let time = Date.parse(timestamp);
